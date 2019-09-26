0bed3613a2213006af6c075139e39047afd24781
Vincent <perrier.vincent0210@gmail.com>
2019-09-25 20:00:27 -0400

    Basic device initialisation. Run through the list of device on the system and opens them all. On branch master Changes to be committed: 	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,29 @@
 
 use rtlsdr;
+use std::vec::Vec;
 
-fn main() 
-{
+fn main() {
+    /* Get the total number of rtlsdr devices on the local machine */
     let number_of_devices = rtlsdr::get_device_count();
 
-    if number_of_devices < 1
-    {
-        println!("There is no rtlsdr device on this system.");
-    }   
-
-    if number_of_devices >= 1
-    {
-        println!("There is no rtlsdr device on this system.");
+    /* this section could be replaced by a match*/
+    if number_of_devices < 1 {
+        panic!("There is no rtlsdr device on this machine.");
     }
 
-    
+    if number_of_devices == 1 {
+        println!(
+            "There is {} rtlsdr device(s) on this machine",
+            number_of_devices
+        );
+    }
+
+    /* open all devices and order them in a vector */
+    let mut device_list: Vec<rtlsdr::RTLSDRDevice> = Vec::new();
+
+    for i in 0..number_of_devices {
+        device_list.push(rtlsdr::open(i).unwrap());
+    }
+
+
 }
